018b5798c2d9df2735d5e8f09dd8eb0bca3b8b5a
Luke Van Seters <lukevanseters@gmail.com>
2014-09-07 15:05:53 -0400

    All templates, styling, routes

--- nublog.py
+++ nublog.py
@@ -1,44 +1,41 @@
 from flask import Flask, request, url_for, redirect, \
-	render_template, abort, g, flash
+	render_template, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 import hashlib
 import uuid
 
 # CONFIG #
-DATABASE = 'sqlite:///./nu_twitter.db'
+DATABASE = 'sqlite:///./nu_blog.db'
 PER_PAGE = 30
 DEBUG = True
 
 # APP #
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['DEBUG'] = DEBUG
-app.config['SECRET_KEY'] = SECRET_KEY
+app.debug = DEBUG
 db = SQLAlchemy(app)
 
 # CONTROLLER #
-
-@app.before_request
-def before_request():
-    g.user = None
-    if 'user_id' in session:
-    	g.user = db.session.query(User).get(session['user_id'])
-
 @app.route('/')
 def home():
     """Shows a feed of blog posts"""
     return render_template('home.html', posts=db.session.query(Post).order_by(Post.pub_date.desc()).limit(PER_PAGE).all())
 
+@app.route('/about')
+def about():
+    """Shows an about page"""
+    return render_template('about.html')
+
 @app.route('/posts/<post_id>')
 def post(post_id):
     """Display's a post"""
-    return render_template('home.html', post=db.session.query(Post).filter_by(id=post_id))
+    return render_template('post.html', post=db.session.query(Post).get(post_id))
 
 # USEFUL METHODS #
-def create_post(user, body, pub_date=None):
+def create_post(title, body, pub_date=None):
     """Creates a post"""
-    p = Post(user, body, pub_date)
+    p = Post(title, body, pub_date)
     db.session.add(p)
     db.session.commit()
     return p
@@ -52,29 +49,31 @@ def format_datetime(timestamp):
     """Format a timestamp for display"""
     return timestamp.strftime('%d/%m/%Y\n%H:%M')
 
+def first_eighty_characters(body):
+    """Returns the first 80 characters of given body"""
+    return body[:81]
+
 # JINJA FILTERS #
 app.jinja_env.filters['datetimeformat'] = format_datetime
 app.jinja_env.filters['gravatar'] = gravatar_url
+app.jinja_env.filters['eighty_chars'] = first_eighty_characters
 
 # MODELS #
-
 class Post(db.Model):
     """A post table"""
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
     body = db.Column(db.Text)
     pub_date = db.Column(db.DateTime)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref=db.backref('posts', lazy='dynamic'))
-
-    def __init__(self, user, body, pub_date=None):
+    def __init__(self, title, body, pub_date=None):
+        self.title = title
         self.body = body
 
         if pub_date is None:
             pub_date = datetime.utcnow()
 
         self.pub_date = pub_date
-        self.user = user
 
     def __repr__(self):
         return '<Post %r>' % self.id
